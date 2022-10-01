40cf717ac81e823385728f837d6b51282aecb95f
Борис <bomen.394@gmail.com>
2022-10-01 15:29:45 +0300

    part 6 - Signup Handler (fix)

--- account/handler/signup.go
+++ account/handler/signup.go
@@ -9,7 +9,7 @@ import (
 )
 
 type signupReq struct {
-	Email    string `json:"email" binding:"required, email"`
+	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,gte=6,lte=30"`
 }
 
