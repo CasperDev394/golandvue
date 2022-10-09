7849686a34a7092e5dfa12af23e4777348607959
Борис <bomen.394@gmail.com>
2022-10-09 15:01:35 +0300

    Part 14 Signin

--- account/handler/handler.go
+++ account/handler/handler.go
@@ -46,14 +46,6 @@ func NewHandler(c *Config) {
 
 }
 
-// Signin handler
-func (h *Handler) Signin(c *gin.Context) {
-	time.Sleep(1 * time.Second)
-	c.JSON(http.StatusOK, gin.H{
-		"hello": "it's signin",
-	})
-}
-
 // Signout handler
 func (h *Handler) Signout(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
